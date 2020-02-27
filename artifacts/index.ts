6cfecb95ae34e4609369aaa6a36cd7e4df3dd987
bweick <brian.weickmann@gmail.com>
2020-02-26 19:49:42 -0800

    Committing folders and new imports for Viewer repo.

---
+++ artifacts/index.ts
@@ -0,0 +1,11 @@
+export { ERC20Viewer } from './ts/ERC20Viewer';
+export { ProtocolViewer } from './ts/ProtocolViewer';
+export { RebalancingSetTokenViewer } from './ts/RebalancingSetTokenViewer';
+export { TradingPoolViewer } from './ts/TradingPoolViewer';
+
+export {
+  ERC20ViewerContract,
+  ProtocolViewerContract,
+  RebalancingSetTokenViewerContract,
+  TradingPoolViewerContract
+} from '../utils/contracts';
---
+++ utils/contracts.ts
@@ -0,0 +1,6 @@
+export { BaseContract } from '../types/base_contract';
+export { ERC20ViewerContract } from '../types/generated/erc20_viewer';
+export { ProtocolViewerContract } from '../types/generated/protocol_viewer';
+export { RebalancingSetTokenViewerContract } from '../types/generated/rebalancing_set_token_viewer';
+export { SocialTradingManagerMockContract } from '../types/generated/social_trading_manager_mock';
+export { TradingPoolViewerContract } from '../types/generated/trading_pool_viewer';
